3e6e7ba04fbc8d72f3eeea2e715e959a72ceb5c4
Hussein <hussein.h.tech@gmail.com>
2022-04-09 15:15:05 +0300

    add toruses

--- main.js
+++ main.js
@@ -3,27 +3,27 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 // Load fonts
-const fontLoader = new FontLoader()
-fontLoader.load('/helvetiker_regular.typeface.json', (font) => {
-    const textGeometry = new TextGeometry("Ramdan Kareem", {
-        font,
-        size: 0.5,
-        height: 0.2,
-        curveSegments: 5,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 3
-    })
-    
-    textGeometry.center()
-    const textMaterial = new THREE.MeshNormalMaterial({ flatShading: true });
-    const text = new THREE.Mesh(textGeometry, textMaterial);
-    scene.add(text)
+const fontLoader = new FontLoader();
+fontLoader.load("/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Ramdan Kareem", {
+    font,
+    size: 2.4,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 3,
+  });
+
+  textGeometry.center();
+  const textMaterial = new THREE.MeshNormalMaterial({ flatShading: true });
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
 });
 
 const canvas = document.querySelector(".webgl");
@@ -42,8 +42,29 @@ scene.background = spaceTexture;
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 26;
 scene.add(camera);
+
+// Torus
+
+const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const torusMaterial = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+
+const torus1 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus2 = new THREE.Mesh(torusGeometry, torusMaterial);
+torus1.position.x = 30;
+torus2.position.x = -30;
+
+scene.add(torus1);
+scene.add(torus2);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
 
 // Controls
 // @ts-ignore
@@ -75,6 +96,14 @@ Array(1200).fill().forEach(addStar);
 
 const tick = () => {
   controls.update();
+
+  torus1.rotation.x += 0.01;
+  torus1.rotation.y += 0.005;
+  torus1.rotation.z += 0.01;
+  
+  torus2.rotation.x += 0.01;
+  torus2.rotation.y += 0.005;
+  torus2.rotation.z += 0.01;
 
   // Render
   renderer.render(scene, camera);
